b8eb639a9f3bd4209ce62c425aeb6c29ac38540b
Seth Acosta <acostavseth@gmail.com>
2026-06-24 13:19:21 +0800

    Add conditionals and printing

--- FutureAgeCalculator/main.swift
+++ FutureAgeCalculator/main.swift
@@ -27,3 +27,15 @@ let currentAge = ageInYear(birthYear: birthYear, targetYear: currentYear)
 let age2050 = ageInYear(birthYear: birthYear, targetYear: 2050)
 let age2100 = ageInYear(birthYear: birthYear, targetYear: 2100)
 let age3000 = ageInYear(birthYear: birthYear, targetYear: 3000)
+
+let yearsUntil100 = 100 - currentAge
+if yearsUntil100 > 0 {
+    print("You will turn 100 in \(yearsUntil100) years.")
+} else {
+    print("You are already over 100 years old!")
+}
+if age3000 >= 1000 {
+    print("Wow! You would be over 1,000 years old in the year 3000.")
+} else {
+    print("You would not reach 1,000 years old by the year 3000.")
+}
