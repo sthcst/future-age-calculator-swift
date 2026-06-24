60de948f7308ce69eecfe4af212f392ec268ffff
Seth Acosta <acostavseth@gmail.com>
2026-06-24 13:15:10 +0800

    Add print and saving for names and age

--- FutureAgeCalculator/main.swift
+++ FutureAgeCalculator/main.swift
@@ -12,3 +12,13 @@ print("Hello, World!")
 func ageInYear(birthYear: Int, targetYear: Int) -> Int {
     return targetYear - birthYear
 }
+
+print("=== Future Age Calculator ===")
+
+print("Enter your name:")
+
+let name = readLine() ?? "Unknown"
+
+print("Enter your birth year:")
+
+let birthYear = Int(readLine() ?? "") ?? 2000
